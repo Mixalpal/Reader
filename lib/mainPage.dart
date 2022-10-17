35e52430ac83ae751ea3db0275152b5dbdd54ee4
Mikhail Panfilov <mixalpalf@gmail.com>
2022-10-17 21:20:37 +0300

    Сверстал навбар

---
+++ lib/mainPage.dart
@@ -0,0 +1,13 @@
+// import 'package:flutter/cupertino.dart';
+
+// class MainPage extends StatefulWidget {
+//   const MainPage({super.key});
+
+//   @override
+//   State<MainPage> createState() => _MainPageState();
+// }
+
+// class _MainPageState extends State<MainPage> {
+//   int _selectedIndex = 0;
+//   String _searchString = "";
+// }
